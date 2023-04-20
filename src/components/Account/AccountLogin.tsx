a11b0bd1ca24fa78f69ae870c7879a754660b24b
0xArcheruss <antonin.dalkolmo@gmail.com>
2023-04-20 11:47:45 +0200

    feat(validation): add rules on login form

--- src/components/Account/AccountLogin.tsx
+++ src/components/Account/AccountLogin.tsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import getDocumentElement from "@popperjs/core/lib/dom-utils/getDocumentElement";
 
 export default function LoginSubmit() {
-  function LoginSubmit() {
-    alert("submit");
-  }
 
+ const [email, setEmail] = useState("")
+  const [password, setPassword] = useState('')
+  const [isValidated, setValidated] = useState(false)
+  const validateForm = (e: React.FormEvent<HTMLFormElement>)=>{
+   e.preventDefault()
+    if(email === ''){
+      alert("Veuillez renseigner un email")
+      setValidated(false)
+    }
+    if(password === ''){
+      alert("Veuillez renseigner votre mot de passe")
+      setValidated(false)
+
+    }
+    if (!email.includes('@')){
+      alert("Veuillez renseigner un email valide")
+      setValidated(false)
+
+    }
+    if(password.length < 8){
+      alert("Votre mot de passe doit faire au moins 8 charactères")
+      setValidated(false)
+    }
+    if(email !== '' && password !== '' && email.includes("@") && password.length>8){
+      setValidated(true);
+    }
+  }
+  useEffect(()=>{
+    if (isValidated){
+      //TODO Login the user with email and password through API
+    }
+  }, [isValidated])
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 mx-auto">
           <div className="my-5">
             <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-            <form onSubmit={() => LoginSubmit()}>
+            <form onSubmit={(e)=>validateForm(e)}>
               <div className="form-group">
                 <label htmlFor="loginEmail">Email</label>
                 <input
@@ -21,6 +51,8 @@ export default function LoginSubmit() {
                   aria-describedby="emailHelp"
                   placeholder="Entrer votre email"
                   autoComplete="email"
+                  value={email}
+                  onChange={(e)=>setEmail(e.target.value)}
                 />
                 <small id="emailHelp" className="form-text text-muted">
                   Nécessite une inscription au préalable
@@ -34,6 +66,8 @@ export default function LoginSubmit() {
                   id="loginPassword"
                   placeholder="Entrer votre mot de passe"
                   autoComplete="current-password"
+                  value={password}
+                  onChange={(e)=>setPassword(e.target.value)}
                 />
               </div>
               <button type="submit" className="btn btn-primary">
