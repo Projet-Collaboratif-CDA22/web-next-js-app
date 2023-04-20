9a0ed16b5c740a11fe5dd38cb463de96adf92482
AlexBOYERE <proalexandreboyere@gmail.com>
2023-04-20 10:21:32 +0200

    feat(accounts): gerer les connexions deconnexions et inscriptions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import {SessionProvider} from "next-auth/react"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps: { session, ...pageProps}}: AppProps) {
+    console.log(pageProps.session);
+    return (
+            <SessionProvider session={pageProps.session}>
+                <Component {...pageProps} />
+            </SessionProvider>
+        );
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,19 +7,14 @@ export const authOptions = {
     CredentialsProvider({
       id: "",
       type: "credentials",
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         username: { label: "Username", type: "text", placeholder: "jsmith" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
         const { username, password } = credentials as any;
-        // Changer le localhost par l'adresse de l'API
+        // TODO: Changer le localhost par l'adresse de l'API
         const res = await fetch(`http://localhost:8000/auth/accounts`, {
           method: "POST",
           headers: {
@@ -42,10 +37,6 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-
-  session: {
-    strategy: "jwt",
-  },
 };
 // @ts-ignore
 export default NextAuth(authOptions);
