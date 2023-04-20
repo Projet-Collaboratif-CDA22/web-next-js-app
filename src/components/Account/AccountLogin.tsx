4324500d9115237bbed285fb693355a436effa80
0xArcheruss <antonin.dalkolmo@gmail.com>
2023-04-20 13:48:40 +0200

    feat(validation): add error handling and validation on forms

--- src/components/Account/AccountLogin.tsx
+++ src/components/Account/AccountLogin.tsx
@@ -1,47 +1,51 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import getDocumentElement from "@popperjs/core/lib/dom-utils/getDocumentElement";
+import Link from "next/link";
 
 export default function LoginSubmit() {
-
- const [email, setEmail] = useState("")
-  const [password, setPassword] = useState('')
-  const [isValidated, setValidated] = useState(false)
-  const validateForm = (e: React.FormEvent<HTMLFormElement>)=>{
-   e.preventDefault()
-    if(email === ''){
-      alert("Veuillez renseigner un email")
-      setValidated(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isValidated, setValidated] = useState(false);
+  const validateForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (email === "") {
+      alert("Veuillez renseigner un email");
+      setValidated(false);
     }
-    if(password === ''){
-      alert("Veuillez renseigner votre mot de passe")
-      setValidated(false)
-
+    if (password === "") {
+      alert("Veuillez renseigner votre mot de passe");
+      setValidated(false);
     }
-    if (!email.includes('@')){
-      alert("Veuillez renseigner un email valide")
-      setValidated(false)
-
+    if (!email.includes("@")) {
+      alert("Veuillez renseigner un email valide");
+      setValidated(false);
     }
-    if(password.length < 8){
-      alert("Votre mot de passe doit faire au moins 8 charactères")
-      setValidated(false)
+    if (password.length < 8) {
+      alert("Votre mot de passe doit faire au moins 8 charactères");
+      setValidated(false);
     }
-    if(email !== '' && password !== '' && email.includes("@") && password.length>8){
+    if (
+      email !== "" &&
+      password !== "" &&
+      email.includes("@") &&
+      password.length > 8
+    ) {
       setValidated(true);
     }
-  }
-  useEffect(()=>{
-    if (isValidated){
+  };
+  useEffect(() => {
+    if (isValidated) {
       //TODO Login the user with email and password through API
+
     }
-  }, [isValidated])
+  }, [isValidated]);
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 mx-auto">
           <div className="my-5">
             <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-            <form onSubmit={(e)=>validateForm(e)}>
+            <form onSubmit={(e) => validateForm(e)}>
               <div className="form-group">
                 <label htmlFor="loginEmail">Email</label>
                 <input
@@ -52,7 +56,7 @@ export default function LoginSubmit() {
                   placeholder="Entrer votre email"
                   autoComplete="email"
                   value={email}
-                  onChange={(e)=>setEmail(e.target.value)}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
                 <small id="emailHelp" className="form-text text-muted">
                   Nécessite une inscription au préalable
@@ -67,7 +71,7 @@ export default function LoginSubmit() {
                   placeholder="Entrer votre mot de passe"
                   autoComplete="current-password"
                   value={password}
-                  onChange={(e)=>setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
               <button type="submit" className="btn btn-primary">
--- src/components/Account/AccountRegister.tsx
+++ src/components/Account/AccountRegister.tsx
@@ -1,92 +1,133 @@
-export default function Register() {
-  function RegisterSubmit() {
-    alert("submit");
-  }
+import {useEffect, useState} from "react";
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6 mx-auto">
-          <div className="my-5">
-            <h1 className="h3 mb-3 font-weight-normal">Please sign up</h1>
-            <form onSubmit={() => RegisterSubmit()}>
-              <div className="form-group">
-                <label htmlFor="username">Nom d&apos;utilisateur</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="username"
-                  aria-describedby="usernameHelp"
-                  placeholder="Entrer votre pseudo"
-                  autoComplete="username"
-                />
-                <small id="usernameHelp" className="form-text text-muted">
-                  Le nom d&apos;utilisateur sera affiché à la vue de tous
-                </small>
-              </div>
-              <div className="form-group">
-                <label htmlFor="lastname">Nom</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="lastname"
-                  placeholder="Entrer votre pseudo"
-                  autoComplete="lastname"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="firstname">Prénom</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="firstname"
-                  placeholder="Entrer votre pseudo"
-                  autoComplete="firstname"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="registerEmail">Email</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="registerEmail"
-                  aria-describedby="emailHelp"
-                  placeholder="Entrer votre email"
-                  autoComplete="email"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                  L&apos;email sera utilisé pour s&apos;identifier
-                </small>
-              </div>
-              <div className="form-group">
-                <label htmlFor="registerPassword">Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="registerPassword"
-                  placeholder="Entrer votre mot de passe"
-                  autoComplete="new-password"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="registerPasswordConfirm">
-                  Password confirmation{" "}
-                </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="registerPasswordConfirm"
-                  placeholder="Entrer votre mot de passe"
-                  autoComplete="new-password"
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                S&apos;inscrire
-              </button>
-            </form>
+export default function Register() {
+  const [userName, setUsername] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastname] = useState("")
+  const [email, setEmail] = useState("")
+  const [passwordFirst, setPasswordFirst] = useState('')
+  const [passwordSecond, setPasswordSecond] = useState('')
+  const [isValidated, setValidated]= useState(false)
+  const validateForm = (e: { preventDefault: () => void; })=>{
+    //Todo add validation
+    e.preventDefault()
+    if(passwordFirst !== passwordSecond){
+      alert("les mots de passes ne correspondent pas")
+      setValidated(false)
+    }else{
+      setValidated(true)
+    }
+console.log(userName,firstName,lastName,email,passwordFirst,passwordSecond)
+  }
+  useEffect(()=>{
+    if (isValidated){
+      //todo: register the user
+    }
+  }, [isValidated])
+ return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mx-auto">
+            <div className="my-5">
+              <h1 className="h3 mb-3 font-weight-normal">Please sign up</h1>
+              <form onSubmit={validateForm}>
+
+                <div className="form-group">
+                  <label htmlFor="username">Nom d&apos;utilisateur</label>
+                  <input
+                      type="text"
+                      className="form-control"
+                      id="username"
+                      aria-describedby="usernameHelp"
+                      placeholder="Entrer votre pseudo"
+                      autoComplete="username"
+                      value={userName}
+                      required={true}
+                      onChange={(e)=>setUsername(e.target.value)
+                  }
+                  />
+                  <small id="usernameHelp" className="form-text text-muted">
+                    Le nom d&apos;utilisateur sera affiché à la vue de tous
+                  </small>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="lastname">Nom</label>
+                  <input
+                      type="text"
+                      className="form-control"
+                      id="lastname"
+                      placeholder="Entrer votre nom"
+                      autoComplete="lastname"
+                      value={lastName}
+                      required={true}
+                      onChange={(e)=>setLastname(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="firstname">Prénom</label>
+                  <input
+                      type="text"
+                      className="form-control"
+                      id="firstname"
+                      placeholder="Entrer votre prénom"
+                      autoComplete="firstname"
+                      value={firstName}
+                      required={true}
+                      onChange={(e)=>setFirstName(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="registerEmail">Email</label>
+                  <input
+                      type="email"
+                      className="form-control"
+                      id="registerEmail"
+                      aria-describedby="emailHelp"
+                      placeholder="Entrer votre email"
+                      autoComplete="email"
+                      value={email}
+                      required={true}
+                      onChange={(e)=>setEmail(e.target.value)}
+                  />
+                  <small id="emailHelp" className="form-text text-muted">
+                    L&apos;email sera utilisé pour s&apos;identifier
+                  </small>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="registerPassword">Password</label>
+                  <input
+                      type="password"
+                      className="form-control"
+                      id="registerPassword"
+                      placeholder="Entrer votre mot de passe"
+                      autoComplete="new-password"
+                      value={passwordFirst}
+                      required={true}
+                      onChange={(e)=>setPasswordFirst(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="registerPasswordConfirm">
+                    Password confirmation
+                  </label>
+                  <input
+                      type="password"
+                      className="form-control"
+                      id="registerPasswordConfirm"
+                      placeholder="Entrer votre mot de passe"
+                      autoComplete="new-password"
+                      required={true}
+                      value={passwordSecond}
+                      onChange={(e)=>setPasswordSecond(e.target.value)}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  S&apos;inscrire
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
