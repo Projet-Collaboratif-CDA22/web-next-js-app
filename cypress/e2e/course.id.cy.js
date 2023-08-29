f593b7cd07a93a955419d50994de2f7f5bc3b957
AlexBOYERE <proalexandreboyere@gmail.com>
2023-08-29 16:27:07 +0200

    feat(course id cy): add test redirect account

---
+++ cypress/e2e/course.id.cy.js
@@ -0,0 +1,12 @@
+describe("Navigation", () => {
+  it("should navigate to the course page id", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000/course");
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="course/30"]').click();
+
+    // The new page should contain an h1 with "Tous les cours"
+    cy.get("button").contains("Se connecter");
+  });
+});
